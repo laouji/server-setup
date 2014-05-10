750465b6686369d5aaa3018ef9d7adcc2e3b6ec5
Crimson Thompson <laouji.rouge@gmail.com>
2014-05-10 17:07:20 +0900

    fastcgi for php

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -1,4 +1,4 @@
-php_packages = %w{ php-common php-cli php-pear }
+php_packages = %w{ php-common php-cli php-pear php-mbstring php-gd php-pear-MDB2-Driver-mysqli php-fpm }
 
 php_packages.each do |pkg|
   package pkg do
@@ -16,21 +16,40 @@ ruby_block "insert_command" do
         "#{k} = #{v}"
       )
     end
+
+    file.search_file_replace_line(
+      /cgi\.fix_pathinfo = [0-9]/,
+      "cgi.fix_pathinfo = 0"
+    )
+
+    file.search_file_replace_line(
+      /;error_log = syslog/,
+      "error_log = syslog"
+    )
+    
     file.write_file
   end
 end
 
+file "#{node[:php][:extension_dir]}/zzz.ini" do
+  owner "root"
+  group "root"
+  mode "644"
+  content "extension=exif.so"
+  action :create
+end
+
 bash "chkconfig --add" do
-  code <<-EOH
-    chkconfig --add mysqld
-    chkconfig mysqld on
-  EOH
-  not_if "chkconfig --list | grep mysqld | grep on"
+    code <<-EOH
+        chkconfig --add php-fpm
+        chkconfig php-fpm on
+    EOH
+    not_if "chkconfig --list | grep php-fpm | grep -q on"
 end
 
 bash "start" do
-  code <<-EOH
-    service mysqld start
-  EOH
-  not_if "service mysqld status"
+    code <<-EOH
+        service php-fpm start
+    EOH
+    not_if "service php-fpm status"
 end

c36335472c83028dda38e10b97a708f99cbb4189
laouji@soixante <laouji.rouge@gmail.com>
2015-06-21 06:51:36 -0400

    fix template and add log directories for nginx openresty

--- site-cookbooks/ngx_openresty/attributes/default.rb
+++ site-cookbooks/ngx_openresty/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:openresty][:version]      = "1.7.10.1"
 default[:openresty][:tarball_path] = "http://openresty.org/download/ngx_openresty-1.7.10.1.tar.gz"
 default[:system][:workdir]         = "/root/work"
-default[:system][:local_lib]       = "/usr/local"
+default[:system][:installdir]      = "/usr/local"
--- site-cookbooks/ngx_openresty/recipes/default.rb
+++ site-cookbooks/ngx_openresty/recipes/default.rb
@@ -1,4 +1,5 @@
 pkgs     = %w{ libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make build-essential }
+dirs     = ['/var/log/nginx', '/etc/nginx/conf.d']
 filename = "ngx_openresty-#{node[:openresty][:version]}"
 
 pkgs.each do |pkg|
@@ -28,16 +29,35 @@ bash "make install" do
     make install
   EOH
   cwd "#{node[:system][:workdir]}"
-  not_if { Dir.exists?("#{node[:system][:local_lib]}/openresty") }
-end
-
-bash "modify permissions" do
-  code <<-EOH
-    chown -R dkr:dkr #{node[:system][:local_lib]}/openresty
-  EOH
+  not_if { Dir.exists?("#{node[:system][:installdir]}/openresty") }
 end
 
 directory "#{node[:system][:workdir]}" do
   recursive true
   action    :delete
+end
+
+dirs.each do |dir|
+  directory dir do
+    user      "nobody"
+    group     "nobody"
+    mode      0755
+    recursive true
+    action    :create
+  end
+end
+
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf"
+  mode   644
+  owner  "nobody"
+  group  "nobody"
+end
+
+bash "add to path and start" do
+  code <<-EOH
+    echo 'PATH=/usr/local/openresty/nginx/sbin:$PATH' >> /root/.bash_profile
+    echo 'export PATH' >> /root/.bash_profile
+    #{node[:system][:installdir]}/openresty/nginx/sbin/nginx -p '#{node[:system][:installdir]}/openresty' -c /etc/nginx/nginx.conf
+  EOH
 end
